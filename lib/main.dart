082efaf9a16d3cbe655c8dcdaa1695536e9fe6e3
BoBoat1 <105755370+BoBoat1@users.noreply.github.com>
2025-01-01 16:06:19 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,14 @@ void main() {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 // how about this
+  // this is funny
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('this is gitlabvvvv'),
+          child: Text('this is gitlabvvvvn'),
         ),
       ),
     );
