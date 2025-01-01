3a69d5cb985c560894658fe462b6e3d49c17912a
sleepapp1995 <sleepapp1995@gmail.com>
2025-01-01 14:40:09 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('this is gitlabvvvv'),
+          child: Text('this is a test fork'),
         ),
       ),
     );
