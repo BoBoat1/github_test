cca82e638d7e220a524696923c932b8cbe186b20
BoBoat1 <105755370+BoBoat1@users.noreply.github.com>
2025-01-01 14:32:24 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('this is a test branch number 2'),
+          child: Text('this is gitlab'),
         ),
       ),
     );
